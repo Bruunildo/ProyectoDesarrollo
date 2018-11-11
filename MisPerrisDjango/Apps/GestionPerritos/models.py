b77813df337e54d7ad1b686edb2718ad5bb3740f
Bruunildo <javier.caro.v@gmail.com>
2018-11-11 09:22:27 -0300

    Se cambia tamano de caracteres a Descripcion perro

--- MisPerrisDjango/Apps/GestionPerritos/models.py
+++ MisPerrisDjango/Apps/GestionPerritos/models.py
@@ -9,7 +9,7 @@ class Perritos(models.Model):
 	tamano = models.FloatField(validators=[MinValueValidator(0.5), MaxValueValidator(2,5)],)
 	peso = models.FloatField(validators=[MinValueValidator(0.1), MaxValueValidator(200)],)
 	color = models.CharField(max_length=20)
-	descripcion = models.CharField(max_length=50)
+	descripcion = models.CharField(max_length=100)
 	fechaRescate = models.DateTimeField()
 	estadoRescate = (('R','Rescatado'),('D','Disponible'),('A','Adoptado'))
 	estado = models.CharField(max_length=1, choices=estadoRescate,default='D')
